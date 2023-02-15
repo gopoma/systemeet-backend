8052165bc0f75d8549ec7993cd8452f8970d3709
gopoma <gordono@unsa.edu.pe>
2023-02-15 17:13:30 -0500

    Added validation components

--- src/dtos/users/dto-types.ts
+++ src/dtos/users/dto-types.ts
@@ -1,26 +1,36 @@
 import { Type } from "@sinclair/typebox";
 
+
 export const firstNameDTOSchema = Type.String({
+    isNotEmpty: true,
     maxLength: 255,
+    transform: ["trim", "toLowerCase"],
     errorMessage: {
         type: "firstName debe de ser del tipo texto",
+        isNotEmpty: "firstName no debe estar vacío",
         maxLength: "firstName debe tener como máximo 255 caracteres de longitud"
     }
 });
 
 export const lastNameDTOSchema = Type.String({
+    isNotEmpty: true,
     maxLength: 255,
+    transform: ["trim", "toLowerCase"],
     errorMessage: {
         type: "lastName debe de ser del tipo texto",
+        isNotEmpty: "lastName no debe estar vacío",
         maxLength: "lastName debe contener como máximo 255 caracteres de longitud"
     }
 });
 
 export const emailDTOSchema = Type.String({
     format: "email",
+    isNotEmpty: true,
+    transform: ["trim", "toLowerCase"],
     errorMessage: {
         type: "email debe de ser del tipo texto",
-        format: "El formato del email no es válido, debe cumplir el RFC 5322"
+        isNotEmpty: "email no debe estar vacío",
+        format: "El formato del email no es válido, debe cumplir el RFC 5321"
     }
 });
 
@@ -34,27 +44,32 @@ export const passwordDTOSchema = Type.String({
     }
 });
 
-export enum Role {
-    REGULAR = "REGULAR",
-    ADMIN = "ADMIN"
-}
-
-// export const roleDTOSchema = Type.Enum(Role, {
-//     message: (value: string) => `role debe ser REGULAR o ADMIN, se recibió ${value}`
-// });
-
-export const birthdayDTOSchema = Type.Date({
+export const roleDTOSchema = Type.String({
+    isNotEmpty: true,
+    enum: ["REGULAR", "ADMIN"],
     errorMessage: {
-        type: "birthday debe de ser del tipo Date",
+        type: "role debe de ser del tipo texto",
+        isNotEmpty: "role no debe estar vacío",
+        enum: "Introduzca un rol válido"
     }
 });
 
-export enum Gender {
-    MALE = "MALE",
-    FEMALE = "FEMALE",
-    NOT_SPECIFIED = "NOT_SPECIFIED"
-}
+export const birthdayDTOSchema = Type.String({
+    isNotEmpty: true,
+    format: "date",
+    errorMessage: {
+        type: "birthday debe de ser del tipo texto",
+        isNotEmpty: "birthday no debe estar vacío",
+        format: "El formato del birthday no es válido, debe cumplir el formato YYYY-MM-DD"
+    }
+});
 
-// export const genderDTOSchema = Type.Enum(Gender, {
-// message: (value: string) => `gender debe ser MALE, FEMALE o NOT_SPECIFIED, se recibió ${value}`
-// });
+export const genderDTOSchema = Type.String({
+    isNotEmpty: true,
+    enum: ["MALE", "FEMALE", "NOT_SPECIFIED"],
+    errorMessage: {
+        type: "gender debe de ser del tipo texto",
+        isNotEmpty: "gender no debe estar vacío",
+        enum: "Introduzca un género válido entre ['MALE', 'FEMALE', 'NOT_SPECIFIED']"
+    }
+});
--- src/libs/validation.ts
+++ src/libs/validation.ts
@@ -1,45 +1,36 @@
-import Ajv from "ajv";
+import Ajv, { KeywordCxt, _ } from "ajv";
 import addFormats from "ajv-formats";
 import addErrors from "ajv-errors";
+import addKeywords from "ajv-keywords";
 
-export function createAjv() {
-    const ajv = new Ajv({ allErrors: true });
-    ajv.addFormat("password", /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/);
-    addFormats(ajv, ["date", "email", "uri"])
+
+const validateNotEmpty = (cxt: KeywordCxt) => {
+    const { data, schema } = cxt;
+    if (schema) {
+        cxt.fail(_`${data}.trim() === ''`);
+    }
+};
+
+export const createAjv = () => {
+    const ajv = new Ajv({ allErrors: true })
         .addKeyword("kind")
-        .addKeyword("modifier");
+        .addKeyword("modifier")
+        .addKeyword({
+            keyword: "isNotEmpty",
+            schemaType: "boolean",
+            type: "string",
+            code: validateNotEmpty,
+            error: { message: "string field must be non-empty" }
+        });
+
+
+    addFormats(ajv, ["date", "email", "uri"])
+        .addFormat("password", /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/);
+
+
     addErrors(ajv);
+    addKeywords(ajv, ["transform"]);
+
 
     return ajv;
-}
-
-import { Type, Static } from "@sinclair/typebox";
-const UserSchema = Type.Object(
-    {
-        birthday: Type.String({
-            format: "date",
-            errorMessage: {
-                type: "Introduzca una fecha >:v",
-                format: "Introduzca una fecha válida >:v"
-            }
-        })
-    },
-    {
-        additionalProperties: false,
-        errorMessage: {
-            type: "envía un objeto >:v",
-            additionalProperties: "No mandes más de lo que se pide >:V",
-            required: {
-                birthday: "Introduzca un birthday >:v"
-            }
-        }
-    }
-);
-type User = Static<typeof UserSchema>
-const user: User = {birthday: "2004-12-32"};
-
-
-const ajv = createAjv();
-const validateSchema = ajv.compile(UserSchema);
-
-console.log(validateSchema(user), "xd", validateSchema.errors);
+};
